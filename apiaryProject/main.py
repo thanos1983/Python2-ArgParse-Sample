c2fa1489a3422ae0296edf484a31079bc1ce9df2
Athanasios Garyfalos <thanos.egw@gmail.com>
2018-05-23 18:27:41 +0200

    Adding documentation and some minor extra tests.

--- apiaryProject/main.py
+++ apiaryProject/main.py
@@ -7,6 +7,11 @@ import validators
 
 
 def url_validation(page):
+    """
+    The function validates the page (url) and raises an error in case of not valid url.
+    :param page: HTML page to validate
+    :return: the same HTML to be used
+    """
     if not validators.url(page):
         page_error = "%r please provide a valid url. Sample: 'https://apiary.docs.apiary.io'" % page
         raise argparse.ArgumentTypeError(page_error)
@@ -14,6 +19,11 @@ def url_validation(page):
 
 
 def file_validation(param):
+    """
+    Check user file input for the extension to check if it is valid.
+    :param param: file input
+    :return: return the name of the file if is passing the validation
+    """
     base, ext = os.path.splitext(param)
     if ext.lower() != '.json':
         file_error = "%r file is not json file. Sample: '<file>.json'" % param
@@ -22,21 +32,35 @@ def file_validation(param):
 
 
 parser = argparse.ArgumentParser(description='Process API')
+# url input -u [html] page
 parser.add_argument('-u', '--url', required=True,
                     type=url_validation,
                     help='Url to GET or POST to API')
+# get input -g [without flag]
 parser.add_argument('-g', '--get', action='store_true',
                     help='Url to retrieve from the API')
+# post input -p [file]
 parser.add_argument('-p', '--post',
                     type=file_validation,
                     help='POST file to the API')
 
 
 def handle_get(html_link):
+    """
+    Get the page based on url REST (GET)
+    :param html_link: page to fetch
+    :return: data fetched
+    """
     return requests.get(html_link)
 
 
 def handle_post(page, file_to_send):
+    """
+    Send json request REST (POST). The function is reading the file and sending in binary format the json data.
+    :param page: url to send the POST request
+    :param file_to_send: JSON file containing the json data
+    :return: the response of the end point
+    """
     path = os.path.join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))), file_to_send)
     headers = {'Authorization': 'Token e61c421a7d9d1de7fd7249b8bdb185f7',
                'Accept': 'application/json',
