801418955eb2eae5a333c4d95183a24a80aafd3e
Athanasios Garyfalos <thanos.egw@gmail.com>
2018-05-23 17:30:59 +0200

    Adding some minor tests and fixing some minor problems.

--- test/test_apiaryProject.py
+++ test/test_apiaryProject.py
@@ -7,23 +7,24 @@ class TestProcessesMethods(unittest.TestCase):
 
     def setUp(self):
         self.page = main.handle_get("https://apiary.docs.apiary.io")
-        # self.post = main.handle_post("https://api.apiary.io/blueprint/publish/publicpersonalapi",
-        #                             "myFile.json")
-        # self.error_content = '{"error":1,"message":"This resource requires authenticated API call."}'
+        self.post = main.handle_post("https://api.apiary.io/blueprint/publish/publicpersonalapi",
+                                     "jsonFile.json")
+        self.error_content = '{"error":1,"message":"This resource requires authenticated API call."}'
 
     def test_get_page_return_code(self):
         self.assertEqual(self.page.status_code, 200)
 
-    # def test_post_page_return_code(self):
-        # self.assertEqual(self.post.status_code, 404)
+    def test_post_page_return_code(self):
+        self.assertEqual(self.post.status_code, 403)
 
-    # def test_post_page_content(self):
-        # self.assertEqual(self.post.content, self.error_content)
+    def test_post_page_content(self):
+        self.assertEqual(self.post.content, self.error_content)
 
     def tearDown(self):
         # empty list (not to keep in memory)
         self.page = None
         self.post = None
+        self.error_content = None
 
 
 if __name__ == '__main__':
